4c7b8f589e6b1923f521aac33944e5c3d50244de
Colten Berry <berrycj@hendrix.edu>
2025-02-04 17:51:20 -0600

    Updated game engine, made progress with hook behavior

---
+++ Fishing Game/Assets/Scripts/HookBehavior.cs
@@ -0,0 +1,53 @@
+using Unity.VisualScripting;
+using UnityEditor.SceneManagement;
+using UnityEngine;
+using UnityEngine.UIElements;
+
+public class HookBehavior : MonoBehaviour
+{
+    public float speed = 1.0f;
+    public int stage = 0;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+
+    }
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        Debug.Log("collision!");
+    }
+    void OnTriggerEnter2D()
+    {
+        Debug.Log("trigger");
+        stage = 2;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey("mouse 0"))
+        {
+            stage = 1;
+            Debug.Log("click!");
+        }
+
+        switch (stage)
+        {
+            case 0:
+                Vector2 pos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+                transform.position = new Vector2(pos.x, transform.position.y);
+                break;
+            case 1:
+                transform.Translate(0, -(speed * Time.deltaTime), 0);
+                break;
+            case 2:
+                transform.Translate(0, speed * Time.deltaTime * 2, 0);
+                if (transform.position.y > 3.1)
+                {
+                    stage = 0;
+                }
+                break;
+
+        }
+    }
+}
